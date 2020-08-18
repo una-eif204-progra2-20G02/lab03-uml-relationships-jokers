8886b1ded662f334f4fea82afa6e01385c21fad2
DayanaBarquero <68917196+DayanaBarquero@users.noreply.github.com>
2020-08-17 21:37:28 -0600

    ready

--- Main.cpp
+++ Main.cpp
@@ -7,17 +7,17 @@
 using namespace std;
 
 int main() {
-    Medicamento Panadol = Medicamento("Panadol", 12.4, 5000, 90, 2030);
-    Medicamento Acetaminofen = Medicamento("Acetaminofen", 5.2, 3000, 75, 2032);
-    Medicamento Vitaminas = Medicamento("Vitaminas", 3.3, 1000, 50, 2023);
-    Tratamiento *PlanSebas = new Tratamiento(14);
-    Tratamiento *PlanGerson = new Tratamiento(30);
-    PlanSebas->ingresarAlFinal(Panadol);
-    PlanSebas->ingresarAlFinal(Acetaminofen);
-    PlanGerson->ingresarAlFinal(Vitaminas);
-    PlanGerson->ingresarAlFinal(Panadol);
-    PacienteAsintomatico *Sebas = new PacienteAsintomatico(31, 05, 20, "Sebastian", "Cambronero", "Mexico","Excelente",PlanSebas);
-    PacienteAsintomatico *Gerson = new PacienteAsintomatico(14, 11, 20, "Gerson", "Hernandez", "Mexico", "Muy Malo",PlanGerson);
+    Medicamento panadol = Medicamento("Panadol", 12.4, 5000, 90, 2030);
+    Medicamento acetaminofen = Medicamento("Acetaminofen", 5.2, 3000, 75, 2032);
+    Medicamento vitaminas = Medicamento("Vitaminas", 3.3, 1000, 50, 2023);
+    Tratamiento *planSebas = new Tratamiento(14);
+    Tratamiento *planGerson = new Tratamiento(30);
+    planSebas->ingresarAlFinal(panadol);
+    planSebas->ingresarAlFinal(acetaminofen);
+    planGerson->ingresarAlFinal(vitaminas);
+    planGerson->ingresarAlFinal(panadol);
+    PacienteAsintomatico *Sebas = new PacienteAsintomatico(31, 05, 20, "Sebastian", "Cambronero", "Mexico","Excelente",planSebas);
+    PacienteAsintomatico *Gerson = new PacienteAsintomatico(14, 11, 20, "Gerson", "Hernandez", "Mexico", "Muy Malo",planGerson);
     cout << Sebas->ToString() << endl;
     cout << Gerson->ToString() << endl;
 
